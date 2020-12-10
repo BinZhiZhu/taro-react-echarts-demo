a66424691a8d029dea8e416937ffb1cf913af460
bin <854767470@qq.com>
2020-12-10 23:33:57 +0800

    perf(del): 忽略打包文件

--- dist_swan/comp.js
+++
@@ -1,2 +0,0 @@
-(swan["webpackJsonp"] = swan["webpackJsonp"] || []).push([["comp"],[],[["./node_modules/@tarojs/mini-runner/dist/template/comp.js","runtime","taro","vendors"]]]);
-//# sourceMappingURL=comp.js.map
